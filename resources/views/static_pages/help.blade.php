593a6fbad40a1f91fec1ff6a81a12a5a05a251ec
vagrant <vagrant@homestead>
2021-07-26 11:18:21 +0000

    basic Page

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.default')
+@section('title','help')
 @section('content')
     <h1>Help</h1>
 @stop
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.default')
+@section('title','home')
 @section('content')
     <h1>home</h1>
 @stop
